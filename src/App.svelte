2ab0666085fd26c8801609d5bd941a903d7c1ba4
Gnlow <entrydark@gmail.com>
2020-10-02 13:11:18 +0900

    redesign

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,38 @@
 <style>
-
+    box {
+        width: 800px;
+        height: 400px;
+        background: #242424;
+        border-radius: 10px;
+        box-shadow: 0 3px 10px rgba(0, 0, 0, 0.2);
+        padding: 35px;
+        box-sizing: border-box; 
+    }
+    identity {
+        display: flex;
+        align-items: center;
+        height: 120px;
+    }
+    identity img {
+        height: 100%;
+    }
+    identity nickname {
+        color: white;
+        font-size: 100px;
+        font-weight: 800;
+        margin-left: 20px;
+    }
 </style>
 
 <script lang="typescript">
 
 </script>
 
-Hello, World!
+<box>
+    <identity>
+        <img alt="" src="https://playentry.org/uploads/profile/57/3f/avatar_573fa113b006225f746e3d4b.png?v=1601610575777"/>
+        <nickname>
+            dark
+        </nickname>
+    </identity>
+</box>
