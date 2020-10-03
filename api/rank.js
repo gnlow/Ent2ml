4a57505846c4debec1b45de6eb324858971d5ba0
Gnlow <entrydark@gmail.com>
2020-10-03 12:33:05 +0900

    fix indent

--- api/rank.js
+++ api/rank.js
@@ -3,52 +3,52 @@ const { getQuery } = helpers
 
 const getData = async () => {
     const staffPicks = await fetch("https://playentry.org/api/rankProject?type=staff&limit=0").then(x => x.json())
-        let users = new Map()
-        staffPicks.forEach(({project}) => {
-            const username = project?.user?.username
-            if (username) {
-                users.set(
-                    username,
-                    [
-                        project.user._id,
-                        (users.get(username)?.[1] || 0) + 1,
-                    ]
-                )
+    let users = new Map()
+    staffPicks.forEach(({project}) => {
+        const username = project?.user?.username
+        if (username) {
+            users.set(
+                username,
+                [
+                    project.user._id,
+                    (users.get(username)?.[1] || 0) + 1,
+                ]
+            )
+        }
+    })
+    return (await Promise.all(Array.from(users.entries())
+        .map(async ([username, [id, count]]) => {
+            const [allData, staffData] = await Promise.all([
+                fetch(`https://playentry.org/api/project/find?&rows=0&tab=my_project&type=project&searching=true&user=${id}&blamed=false`).then(x => x.json()),
+                fetch(`https://playentry.org/api/project/find?&sort=staffPicked&tab=my_project&type=project&searching=true&user=${id}&blamed=false`).then(x => x.json())
+            ])
+
+            const {visitCount, likeCount, recentLikeCount, commentCount} = allData.data.reduce(({visitCount, likeCount, recentLikeCount, commentCount}, {visit, likeCnt, recentLikeCnt, comment}) => {
+                visitCount += visit
+                likeCount += likeCnt
+                recentLikeCount += recentLikeCnt
+                commentCount += comment
+                return {visitCount, likeCount, recentLikeCount, commentCount}
+            }, {
+                visitCount: 0,
+                likeCount: 0,
+                recentLikeCount: 0,
+                commentCount: 0,
+            })
+
+            return {
+                username,
+                id,
+                staffCount: Math.max(
+                    count,
+                    staffData.data.length
+                ),
+                visitCount,
+                likeCount,
+                recentLikeCount,
+                commentCount,
             }
-        })
-        return (await Promise.all(Array.from(users.entries())
-            .map(async ([username, [id, count]]) => {
-                const [allData, staffData] = await Promise.all([
-                    fetch(`https://playentry.org/api/project/find?&rows=0&tab=my_project&type=project&searching=true&user=${id}&blamed=false`).then(x => x.json()),
-                    fetch(`https://playentry.org/api/project/find?&sort=staffPicked&tab=my_project&type=project&searching=true&user=${id}&blamed=false`).then(x => x.json())
-                ])
-
-                const {visitCount, likeCount, recentLikeCount, commentCount} = allData.data.reduce(({visitCount, likeCount, recentLikeCount, commentCount}, {visit, likeCnt, recentLikeCnt, comment}) => {
-                    visitCount += visit
-                    likeCount += likeCnt
-                    recentLikeCount += recentLikeCnt
-                    commentCount += comment
-                    return {visitCount, likeCount, recentLikeCount, commentCount}
-                }, {
-                    visitCount: 0,
-                    likeCount: 0,
-                    recentLikeCount: 0,
-                    commentCount: 0,
-                })
-
-                return {
-                    username,
-                    id,
-                    staffCount: Math.max(
-                        count,
-                        staffData.data.length
-                    ),
-                    visitCount,
-                    likeCount,
-                    recentLikeCount,
-                    commentCount,
-                }
-            })))
+        })))
 }
 
 const app = new Application
