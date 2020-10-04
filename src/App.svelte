e28a3c151f5aba7211d926bceb8e175fde9d5086
Gnlow <entrydark@gmail.com>
2020-10-04 17:51:40 +0900

    add loader

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,6 @@
         box-shadow: 0 3px 10px rgba(0, 0, 0, 0.2);
         padding: 35px;
         box-sizing: border-box;
-        color: white;
     }
     identity {
         display: flex;
@@ -22,7 +21,6 @@
         border-radius: 50%;
     }
     identity nickname {
-        color: white;
         font-size: 100px;
         font-weight: 800;
         margin-left: 20px;
@@ -34,6 +32,20 @@
             "visit visit visit comment comment comment"   50%
             "like like recentLike recentLike staff staff" 50%;
         font-size: 64px;
+    }
+    loader {
+        border: 16px solid #242424;
+        border-top: 16px solid white;
+        border-radius: 50%;
+        width: 120px;
+        height: 120px;
+        animation: spin 2s linear infinite;
+    }
+    @keyframes spin {
+        100% { transform: rotate(360deg); }
+    }
+    info {
+        font-size: 50px;
     }
 </style>
 
@@ -48,19 +60,29 @@ onMount(async () => {
 })
 </script>
 {#if user}
-    <box>
-        <identity>
-            <img alt="{user.username}" src="https://playentry.org/uploads/profile/{user.id.substring(0, 2)}/{user.id.substring(2, 4)}/avatar_{user.id}.png"/>
-            <nickname>
-                {user.username}
-            </nickname>
-        </identity>
-        <scores>
-            <Item {user} icon="visit"/>
-            <Item {user} icon="comment"/>
-            <Item {user} icon="like"/>
-            <Item {user} icon="recentLike"/>
-            <Item {user} icon="staff"/>
-        </scores>
-    </box>
+    {#if !user.notUser}
+        <box>
+            <identity>
+                <img alt="{user.username}" src="https://playentry.org/uploads/profile/{user.id.substring(0, 2)}/{user.id.substring(2, 4)}/avatar_{user.id}.png"/>
+                <nickname>
+                    {user.username}
+                </nickname>
+            </identity>
+            <scores>
+                <Item {user} icon="visit"/>
+                <Item {user} icon="comment"/>
+                <Item {user} icon="like"/>
+                <Item {user} icon="recentLike"/>
+                <Item {user} icon="staff"/>
+            </scores>
+        </box>
+    {:else}
+        <info>
+            존재하지 않는 사용자입니다.<br/>
+            ID를 올바르게 입력하셨는지 확인해주세요! :D
+        </info>
+        
+    {/if}
+{:else}
+    <loader/>
 {/if}
