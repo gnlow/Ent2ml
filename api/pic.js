38cfb1b3ad88910ec10fb46d3727ed9570f5e5fd
Gnlow <entrydark@gmail.com>
2020-10-21 11:37:59 +0900

    Fix #9

--- api/pic.js
+++ api/pic.js
@@ -3,10 +3,19 @@ const { getQuery } = helpers
 
 const app = new Application
 
+const getDefaultPic = async () => await fetch("https://playentry.org/img/assets/avatar_img.png")
+
 app.use(async ctx => {
     try {
         const { id } = getQuery(ctx)
-        const res = await fetch(`https://playentry.org/uploads/profile/${id.substring(0, 2)}/${id.substring(2, 4)}/avatar_${id}.png`).then(res => res.arrayBuffer())
+        const res = await fetch(`https://playentry.org/uploads/profile/${id.substring(0, 2)}/${id.substring(2, 4)}/avatar_${id}.png`)
+            .then(async res => 
+                (
+                    res.ok 
+                    ? res
+                    : await getDefaultPic()
+                ).arrayBuffer()
+            )
         ctx.response.body = new Uint8Array(res)
         ctx.response.headers.set("Content-Type", "image/png")
     } catch (e) {
