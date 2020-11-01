45e7a31881edeb6d848ccc60bb4a452782565c40
Gnlow <entrydark@gmail.com>
2020-11-01 16:14:58 +0900

    enable typescript

--- rollup.config.js
+++ rollup.config.js
@@ -4,70 +4,39 @@ import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 
+import preprocess from 'svelte-preprocess';
+
 const production = !process.env.ROLLUP_WATCH;
 
+function basicConf(filename) {
+  return {
+    input: `src/${filename}.js`,
+    output: {
+      sourcemap: true,
+      format: 'iife',
+      name: 'app',
+      file: `public/${filename}.js`,
+    },
+    plugins: [
+      svelte({
+        dev: !production,
+        preprocess: preprocess(),
+        css: css => {
+          css.write(`public/${filename}.css`);
+        },
+      }),
+      resolve({ browser: true }),
+      commonjs(),
+      !production && livereload('public'),
+      production && terser(),
+    ],
+    watch: {
+      clearScreen: false,
+    },
+  }
+};
+
 export default [
-  {
-    input: 'src/user.js',
-    output: {
-      sourcemap: true,
-      format: 'iife',
-      name: 'app',
-      file: 'public/user.js',
-    },
-    plugins: [
-      svelte({
-        // enable run-time checks when not in production
-        dev: !production,
-        // we'll extract any component CSS out into
-        // a separate file — better for performance
-        css: css => {
-          css.write('public/user.css');
-        },
-      }),
-
-      // If you have external dependencies installed from
-      // npm, you'll most likely need these plugins. In
-      // some cases you'll need additional configuration —
-      // consult the documentation for details:
-      // https://github.com/rollup/rollup-plugin-commonjs
-      resolve({ browser: true }),
-      commonjs(),
-
-      // Watch the `public` directory and refresh the
-      // browser on changes when not in production
-      !production && livereload('public'),
-
-      // If we're building for production (npm run build
-      // instead of npm run dev), minify
-      production && terser(),
-    ],
-    watch: {
-      clearScreen: false,
-    },
-  },
-  {
-    input: 'src/home.js',
-    output: {
-      sourcemap: true,
-      format: 'iife',
-      name: 'app',
-      file: 'public/home.js',
-    },
-    plugins: [
-      svelte({
-        dev: !production,
-        css: css => {
-          css.write('public/home.css');
-        },
-      }),
-      resolve({ browser: true }),
-      commonjs(),
-      !production && livereload('public'),
-      production && terser(),
-    ],
-    watch: {
-      clearScreen: false,
-    },
-  },
+  basicConf("home"),
+  basicConf("user"),
 ];
