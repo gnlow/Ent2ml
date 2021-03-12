e7e645a2d71d63cfd5f8b3f62ae395f1e86af31e
Gnlow <entrydark@gmail.com>
2021-03-12 10:36:27 +0900

    개발중 공지

--- src/HomeApp.svelte
+++ src/HomeApp.svelte
@@ -1,9 +1,16 @@
 <style>
-
+    #logo {
+        object-fit: cover;
+        width: 100%;
+        max-width: 300px;
+    }
 </style>
 
 <script lang="typescript">
-    import Search from "./Search.svelte"
+
 </script>
 
-<Search/>
+<img id="logo" alt="Ent2ml logo" src="/img/logo.png"/>
+<div style="font-size: 48px;">
+    리뉴얼된 엔트리에 맞춰 업데이트하는 중입니다.
+</div>
